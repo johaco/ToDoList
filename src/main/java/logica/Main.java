5801cde0f9639698681ff705ab5b7a58db06f94d
johaco <johaco@johaco-EXO-Smart-XQ3C>
2023-06-22 17:36:08 -0300

    Carga Iniciar

---
+++ src/main/java/logica/Main.java
@@ -0,0 +1,16 @@
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author johaco
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
